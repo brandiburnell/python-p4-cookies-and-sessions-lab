5d2f7e0d0ea9c4833c5174d1b264700103d38cd6
Brandi Burnell <brandiburnell6@gmail.com>
2024-04-20 14:40:34 -0600

    pass all tests

--- server/app.py
+++ server/app.py
@@ -22,13 +22,22 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
+    articles = [article.to_dict() for article in Article.query.all()]
 
-    pass
+    return make_response(jsonify(articles), 200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] = session['page_views'] + 1
 
-    pass
+    print(session['page_views'])
+
+    if session['page_views'] < 4:
+        article = Article.query.filter_by(id=id).first()
+        return make_response(jsonify(article.to_dict()), 200)
+
+    return {'message': 'Maximum pageview limit reached'}, 401
 
 if __name__ == '__main__':
     app.run(port=5555)
